2e072034a216abd053b78ffe29b9e3e1c1aeceb6
sinan <sinan.lakhani@hotmail.com>
2020-06-24 02:28:07 -0700

    Updates

---
+++ precache-manifest.8fb3c166e9538c7d112cdf951a94c5d7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc706f2fc2f9b80943832894a2126af",
+    "url": "/github.io/horizon/index.html"
+  },
+  {
+    "revision": "54398725babdfc9d5617",
+    "url": "/github.io/horizon/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3984b4e6798da640036d",
+    "url": "/github.io/horizon/static/css/main.61b28c0f.chunk.css"
+  },
+  {
+    "revision": "54398725babdfc9d5617",
+    "url": "/github.io/horizon/static/js/2.5bd9b199.chunk.js"
+  },
+  {
+    "revision": "df835edbe6947b41c9ad692616389269",
+    "url": "/github.io/horizon/static/js/2.5bd9b199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3984b4e6798da640036d",
+    "url": "/github.io/horizon/static/js/main.2c6ae832.chunk.js"
+  },
+  {
+    "revision": "2700e3fce5c6a8935883",
+    "url": "/github.io/horizon/static/js/runtime-main.2d52ebbd.js"
+  },
+  {
+    "revision": "d44ee16c4c56b31517333e0d9b6293c1",
+    "url": "/github.io/horizon/static/media/ihorizon.d44ee16c.png"
+  },
+  {
+    "revision": "9305a29551b93a7aea398611aa88c5e6",
+    "url": "/github.io/horizon/static/media/logo1.9305a295.png"
+  },
+  {
+    "revision": "9042f8b7abc6afd0bb3702c817541030",
+    "url": "/github.io/horizon/static/media/logo2.9042f8b7.png"
+  },
+  {
+    "revision": "66a2fd0440dbebeabf47999e032a241a",
+    "url": "/github.io/horizon/static/media/logo3.66a2fd04.png"
+  },
+  {
+    "revision": "ffcf8cae240c8e928ebc75c4055bbacf",
+    "url": "/github.io/horizon/static/media/logo4.ffcf8cae.png"
+  }
+]);
